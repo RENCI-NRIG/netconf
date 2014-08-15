d4e7f46965fabdad685242d84aef6895ba6af120
Ilya Baldin <ibaldin@users.noreply.github.com>
2014-08-15 18:12:42 +0000

    Netconf library

---
+++ src/main/java/net/juniper/netconf/CommitException.java
@@ -0,0 +1,32 @@
+/**
+* Copyright (c) 2013 Juniper Networks, Inc.
+* All Rights Reserved
+*
+* Use is subject to license terms.
+*
+*/
+
+package net.juniper.netconf;
+
+import java.io.IOException;
+
+/**
+ * Describes exceptions related to commit operation
+ */
+public class CommitException extends IOException {
+    
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public final String CommitErrorMsg;
+ 
+    CommitException(String msg) {
+        super(msg);
+        CommitErrorMsg = msg;
+    }
+
+    public String getCommitErrorMessage() {
+        return CommitErrorMsg;
+    }
+}
